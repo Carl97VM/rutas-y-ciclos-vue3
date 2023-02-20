bdff3716ce7eb39aac8c2b97de6541b6e32b23d5
Carl97VM <barcacarlosv@gmail.com>
2023-02-20 12:59:04 -0400

    Router y Layouts de dos modulos

--- src/modulos/pokemon/pages/AboutPage.vue
+++ src/modulos/pokemon/pages/AboutPage.vue
@@ -1,10 +1,41 @@
 <template>
   <h1>About Page</h1>
+  <hr>
+  <h2>{{ name }}</h2>
+  <button @click="ChangeName">Change Name</button>
 </template>
 
 <script>
-export default {
 
+export default {
+    data() {
+      return {
+        name: 'Juan'
+      }
+    },
+    methods: {
+      ChangeName: function() {
+        this.name = 'Carlos'
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreate')
+    },
+    created() {
+      console.log('created')
+    },
+    beforeMount() {
+      console.log('beforeMount')
+    },
+    mounted() {
+      console.log('mounted')
+    },
+    beforeUpdate() {
+      console.log('beforeUpdate')
+    },
+    updated() {
+      console.log('updated')
+    },
 }
 </script>
 
